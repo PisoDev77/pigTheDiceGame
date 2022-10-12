d98af027da6e4bc69ad8da57c5fb4cbc6be57352
PisoDev77 <pisodev0427@gmail.com>
2022-10-12 16:28:47 +0900

    feat: Render current Score & New Btn process
    Reload when new btn clicked
    
    Render current score by app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,22 @@
 import handleDice from "./handleDice.js";
 
 (()=>{
-
+    
+    let current_player = handleDice.p1;
+    
     const btns = document.querySelector(".btn-wrapper");
     btns.addEventListener("click", (e)=>{
         
         const btn = e.target.classList;
-        console.log(btn);
         
         if(btn.contains("roll-btn")){
-            console.log("THIS IS ROLL");
+            const rand = handleDice.rollingDice(current_player);
+            const ele = document.querySelector(`.${current_player.name}`).querySelector(".current-score");
+            ele.innerText = rand;
+
+            if(current_player.score === 0 ){
+                current_player = handleDice.p2;
+            }
         }
 
         if(btn.contains("save-btn")){
@@ -17,7 +24,8 @@ import handleDice from "./handleDice.js";
         }
 
         if(btn.contains("new-btn")){
-            console.log("THIS IS NEW");
+            location.reload();
+            current_player = handleDice.p1;
         }
         
     });
