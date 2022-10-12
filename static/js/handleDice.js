d62596d251591932e006a6c62baf96d4c7093268
PisoDev77 <pisodev0427@gmail.com>
2022-10-12 15:25:21 +0900

    feat: Set methods in handleDice.js
    I added 3 methods
    - rollingDice
    - savingDice
    - initializing

---
+++ static/js/handleDice.js
@@ -0,0 +1,39 @@
+// class나 prototype이 좋으나, 시간관계상 p1 p2
+
+const p1 = {
+    name:"player 1",
+    score: 0,
+}
+
+const p2 ={
+    ame:"player 2",
+    score:0,
+}
+
+const rollingDice = (player) => {
+
+    const rand = parseInt(Math.random() * (6-1) + 1);
+
+    if(rand === 1){
+        initializingScore(player);
+    }else{
+        savingDice(player, rand);
+    }
+    
+};
+
+const savingDice = (player, score) => {
+    player.score += score; 
+};
+
+const initializingScore = (player) => {
+    player.score = 0;
+};
+
+//test IIFE by console
+(()=>{
+    rollingDice(p1);
+    rollingDice(p2);
+    
+    console.log(p1, p2);
+})();
